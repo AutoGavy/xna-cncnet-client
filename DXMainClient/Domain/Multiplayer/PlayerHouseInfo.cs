f93e6f35f96cc02c646519da79df8808ebf756e0
Sad Pencil <qh06@qq.com>
2022-03-06 01:10:44 +0800

    尝试添加阵营限制规则（未测试）

--- DXMainClient/Domain/Multiplayer/PlayerHouseInfo.cs
+++ DXMainClient/Domain/Multiplayer/PlayerHouseInfo.cs
@@ -6,30 +6,8 @@ namespace DTAClient.Domain.Multiplayer
 {
     public class PlayerHouseInfo
     {
-        int tempSideInedex;
-        static public readonly int[] sideIndexReal = {
-            0,  // 0
-            1,  // 1
-            2,  // 2
-            10, // 3
-            3,  // 4
-            4,  // 5
-            5,  // 6
-            11, // 7
-            6,  // 8
-            7,  // 9
-            8,  // 10
-            9,  // 11
-            12,
-            13,
-            14,
-            15,
-            16,
-            17,
-            18
-        };
-
-        public int SideIndex { get { return sideIndexReal[tempSideInedex]; } set { tempSideInedex = value; } }
+        public int SideIndex { get; set; }
+        //public int SideIndex { get { return sideIndexReal[tempSideInedex]; } set { tempSideInedex = value; } }
 
         /// <summary>
         /// A side (or, more correctly, house or country depending on the game)
--- DXMainClient/Domain/SavedGame.cs
+++ DXMainClient/Domain/SavedGame.cs
@@ -15,22 +15,6 @@ namespace DTAClient.Domain
         const string SAVED_GAME_PATH = "Saved Games/";
         const string findSample = ".MAP";
 
-        private readonly Dictionary<string, string> SideList = new Dictionary<string, string>
-        {
-            {"GDI-Offense", "GDI"},
-            {"GDI-Defense", "GDI"},
-            {"GDI-Support", "GDI"},
-            {"ZOCOM",       "GDI"},
-            {"Nod-Offense", "Nod"},
-            {"Nod-Defense", "Nod"},
-            {"Nod-Support", "Nod"},
-            {"MarkedOfKane","Nod"},
-            {"Scrin",       "Scrin"},
-            {"Reaper17",    "Scrin"},
-            {"Traveler59",  "Scrin"},
-            {"Destroyer41", "Scrin"}
-        };
-
         public SavedGame(string fileName)
         {
             FileName = fileName;
@@ -111,11 +95,11 @@ namespace DTAClient.Domain
                     if (String.IsNullOrEmpty(strName))
                         return false;
 
-                    if (!SideList.ContainsKey(strName))
+                    if (!GS_Side.SideToFaction.ContainsKey(strName))
                         return false;
 
-                    Logger.Log("SavedGame: Player side is " + SideList[strName]);
-                    SideName = SideList[strName];
+                    Logger.Log("SavedGame: Player side is " + GS_Side.SideToFaction[strName]);
+                    SideName = GS_Side.SideToFaction[strName];
                     return true;
                 }
             }
