301bc6aaa1b0f3417c4d5fb4c9a385a876d2e4ac
SadPencil <sadpencil@outlook.com>
2025-02-02 21:06:56 +0800

    Move IME handler to client

--- DXMainClient/DXGUI/GameClass.cs
+++ DXMainClient/DXGUI/GameClass.cs
@@ -2,6 +2,7 @@
 using ClientCore.CnCNet5;
 using DTAClient.Domain;
 using DTAClient.DXGUI.Generic;
+using DTAClient.IME;
 using Localization;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -102,6 +103,7 @@ namespace DTAClient.DXGUI
 
             WindowManager wm = new WindowManager(this, graphics);
             wm.Initialize(content, ProgramConstants.GetBaseResourcePath());
+            wm.IMEHandler = IMEHandler.Create(this);
 
             SetGraphicsMode(wm);
 
---
+++ DXMainClient/IME/DummyIMEHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace DTAClient.IME
+{
+    internal class DummyIMEHandler : IMEHandler
+    {
+        public DummyIMEHandler() { }
+
+        public override bool TextCompositionEnabled { get => false; protected set { } }
+
+        public override void SetTextInputRectangle(Rectangle rectangle) { }
+        public override void StartTextComposition() { }
+        public override void StopTextComposition() { }
+    }
+}
